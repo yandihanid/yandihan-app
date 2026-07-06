450b9e1f8ede933aa8beccbc722fcb3a260ebe5a
Your Name <yandihan@proton.me>
2026-07-06 09:58:45 +0700

    fix error supabase

--- utils/supabase/client.js
+++ utils/supabase/client.js
@@ -1,1 +1,9 @@
 // Tidak ada perubahan untuk saat ini
+import { createBrowserClient } from '@supabase/ssr'
+
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  )
+}
